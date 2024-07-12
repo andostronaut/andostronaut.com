8aaf9eddf57f81c9d7cb1d4855eb4233a4021d4e
Ando <65276708+andostronaut@users.noreply.github.com>
2024-07-12 18:18:46 +0300

    feat: update some info on the resume

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -2,28 +2,32 @@ import { Icons } from '@/components/icons'
 import { CodeIcon, HomeIcon, NotebookIcon, PencilLine } from 'lucide-react'
 
 export const DATA = {
-  name: 'Dillion Verma',
-  initials: 'DV',
-  url: 'https://dillion.io',
-  location: 'San Francisco, CA',
-  locationLink: 'https://www.google.com/maps/place/sanfrancisco',
+  name: 'Ando RAMANAMIHANTA',
+  initials: 'AR',
+  url: 'https://andostronaut.com',
+  location: 'Antananarivo, MG',
+  locationLink: 'https://www.google.com/maps/place/antananarivo',
   description:
-    'Software Engineer turned Entrepreneur. I love building things and helping people. Very active on Twitter.',
+    'Software Engineer and Open Source Developer passioned about engineering stuff and sharing knowledge to make this world more better 🌱',
   summary:
     'At the end of 2022, I quit my job as a software engineer to go fulltime into building and scaling my own SaaS businesses. In the past, [I pursued a double degree in computer science and business](/#education), [interned at big tech companies in Silicon Valley](https://www.youtube.com/watch?v=d-LJ2e5qKdE), and [competed in over 21 hackathons for fun](/#hackathons). I also had the pleasure of being a part of the first ever in-person cohort of buildspace called [buildspace sf1](https://buildspace.so/sf1).',
-  avatarUrl: '/me.png',
+  avatarUrl: '/me.jpg',
   skills: [
     'React',
     'Next.js',
     'Typescript',
+    'Deno',
     'Node.js',
     'Python',
     'Go',
     'Postgres',
     'Docker',
-    'Kubernetes',
-    'Java',
-    'C++',
+    'Rust',
+    'Vue.js',
+    'Express.js',
+    'Bun',
+    'GraphQL',
+    'Supabase',
   ],
   navbar: [
     { href: '/', icon: HomeIcon, label: 'Home' },
